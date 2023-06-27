a00e2f56cbf4ee9c66ebb3860c8cfe9c18463fd2
ekko <chennathan@163.com>
2023-06-27 15:01:02 +0800

    eslint_format

--- src/components/rdkitComponent/tagMols.vue
+++ src/components/rdkitComponent/tagMols.vue
@@ -1,43 +1,45 @@
 <script setup lang="ts">
-import { NTag,NSelect, type SelectRenderTag } from "naive-ui";
-import { ref,computed,onMounted,h } from "vue";
+import { NSelect, NTag } from 'naive-ui'
+import { computed, h, onMounted } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { useEnumStore } from '@/stores/enumStore'
-const props=defineProps<{tags:string[]}>()
-const emit=defineEmits(['update:tags'])
-const tagsValue = useVModel(props,'tags',emit)
+
+const props = defineProps<{ tags: string[] }>()
+const emit = defineEmits(['update:tags'])
+const tagsValue = useVModel(props, 'tags', emit)
 
 const enumStore = useEnumStore()
-const tagsOption=computed<{label:string,value:string}[]>(()=>{
-	return enumStore.getAllLabels.map((v:string)=>({label:v,value:v}))
+const tagsOption = computed<{ label: string; value: string }[]>(() => {
+  return enumStore.getAllLabels.map((v: string) => ({ label: v, value: v }))
 })
-const renderTag=({ option, handleClose })=>{
-	return h(NTag,
-        	{
-						type:'success',
-        	  closable:true,
-						round:true,
-						size:"small",
-        	  onMousedown: (e:FocusEvent)=>{ 
-							e.preventDefault() 
-						},
-					 	onClose: (e:MouseEvent)=>{
-        	    e.stopPropagation()
-        	    handleClose()
-        	  }
-					},
-        	{ default: () => option.label } )
+function renderTag({ option, handleClose }) {
+  return h(NTag, {
+    type: 'success',
+    closable: true,
+    round: true,
+    size: 'small',
+    onMousedown: (e: FocusEvent) => {
+      e.preventDefault()
+    },
+    onClose: (e: MouseEvent) => {
+      e.stopPropagation()
+      handleClose()
+    },
+  },
+  { default: () => option.label })
 }
-onMounted(()=>{
-	
+onMounted(() => {
+
 })
 </script>
+
 <template>
-	<n-select type="info" 
-						v-model:value="tagsValue"
-						size="small" 
-						:options="tagsOption"
-						:render-tag="renderTag"
-						multiple clearable filterable tag >
-	</n-select>
+  <NSelect
+    v-model:value="tagsValue"
+    type="info"
+    size="small"
+    :options="tagsOption"
+    :render-tag="renderTag"
+    multiple clearable filterable tag
+  />
 </template>
